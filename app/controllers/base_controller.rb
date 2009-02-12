a039811c4d2de7e1c507ba4e1f1116ed0d4fd85e
Justin Geibel <jtgeibel@server.local>
2009-02-11 20:07:06 -0500

    Turning off caching on the front page.  I should really fix it so that updatings posts clears this cache.

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -11,7 +11,7 @@ class BaseController < ApplicationController
   before_filter :initialize_header_tabs
   before_filter :initialize_admin_tabs
 
-  caches_action :site_index, :footer_content, :if => Proc.new{|c| c.cache_action? }
+#  caches_action :site_index, :footer_content, :if => Proc.new{|c| c.cache_action? }
   def cache_action?
     !logged_in? && controller_name.eql?('base') && params[:format].blank? 
   end  
