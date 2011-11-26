bbf18cc09f978ff5b09bdfdb56ee5b23afef897d
Justin Geibel <jtgeibel@gmail.com>
2011-11-25 20:50:22 -0500

    Wrong link was being sent in emails.  Changing more http references to https protocol.

--- app/models/user_notifier.rb
+++ app/models/user_notifier.rb
@@ -4,7 +4,8 @@ class UserNotifier < ActionMailer::Base
   extend  ActionView::Helpers::SanitizeHelper::ClassMethods # Required for rails 2.2
 
   include BaseHelper
-  ActionMailer::Base.default_url_options[:host] = APP_URL.sub('http://', '')
+  ActionMailer::Base.default_url_options[:host] = APP_URL.sub('https://', '')
+  ActionMailer::Base.default_url_options[:protocol] = "https"
 
   def signup_invitation(email, user, message)
     setup_sender_info
--- config/initializers/rakismet.rb
+++ config/initializers/rakismet.rb
@@ -1,5 +1,5 @@
 if AppConfig.akismet_key
   Rakismet::KEY  = AppConfig.akismet_key
-  Rakismet::URL  = APP_URL.gsub("http://", '')
+  Rakismet::URL  = APP_URL.gsub("https://", '')
   Rakismet::HOST = 'rest.akismet.com'
 end
